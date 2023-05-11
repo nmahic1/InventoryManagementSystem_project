15d10db74d47f7536aadf3321f1301192142c9e4
nmahic1 <nmahic1@etf.unsa.ba>
2023-05-11 19:17:26 +0200

    Changes in product fxml and controller

--- src/main/java/controller/ProductController.java
+++ src/main/java/controller/ProductController.java
@@ -1,4 +1,38 @@
 package controller;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Menu;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class ProductController {
+
+    @FXML
+    private Menu brandAndProduct;
+
+    @FXML
+    private Menu product;
+
+    @FXML
+    private Menu cashRegister;
+
+    @FXML
+    private Menu logOut;
+
+    public void logout(javafx.event.ActionEvent actionEvent) throws IOException {
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/LogIn.fxml"));
+        Parent root = loader.load();
+
+        // Otvaranje nove scene s drugim prozorom
+        Scene scene = new Scene(root);
+        Stage stage = new Stage();
+        stage.setScene(scene);
+        stage.show();
+
+    }
 }
