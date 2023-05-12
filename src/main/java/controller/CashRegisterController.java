d9879592bcd93f3df3cd1f77ad580afa5188f5d3
nmahic1 <nmahic1@etf.unsa.ba>
2023-05-12 15:37:05 +0200

    Making invoice button

--- src/main/java/controller/CashRegisterController.java
+++ src/main/java/controller/CashRegisterController.java
@@ -1,4 +1,29 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class CashRegisterController {
+
+
+    public Button payinvoice;
+
+
+    public void PayInvoiceBitton(ActionEvent actionEvent) throws IOException {
+        // Code to handle save menu item
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/Invoice.fxml"));
+        Parent root = loader.load();
+
+        // Otvaranje nove scene s drugim prozorom
+        Scene scene = new Scene(root);
+        Stage stage = new Stage();
+        stage.setScene(scene);
+        stage.show();
+    }
 }
