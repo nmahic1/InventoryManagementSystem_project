660d10d16926ad78e60841877511835a9fb4d0bb
nmahic1 <nmahic1@etf.unsa.ba>
2023-05-21 15:21:43 +0200

    fixing menu bar for cash register

--- src/main/java/controller/CashRegisterController.java
+++ src/main/java/controller/CashRegisterController.java
@@ -1,19 +1,68 @@
 package controller;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Menu;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class CashRegisterController {
 
+    @FXML
+    private Menu brandAndProduct;
+
+    @FXML
+    private Menu product;
+
+
+    @FXML
+    private Menu logOut;
 
     public Button payinvoice;
 
+    @FXML
+    void handleProduct(javafx.event.ActionEvent actionEvent) throws IOException{
+        // Code to handle save menu item
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/Product.fxml"));
+        Parent root = loader.load();
+
+        // Otvaranje nove scene s drugim prozorom
+        Scene scene = new Scene(root);
+        Stage stage = new Stage();
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    void handlebrandAndCategory(javafx.event.ActionEvent actionEvent) throws IOException{
+        // Code to handle exit menu item
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/BrandAndProduct.fxml"));
+        Parent root = loader.load();
+
+        // Otvaranje nove scene s drugim prozorom
+        Scene scene = new Scene(root);
+        Stage stage = new Stage();
+        stage.setScene(scene);
+        stage.show();
+        //isto
+    }
+
+    @FXML
+    void logout(javafx.event.ActionEvent actionEvent) throws IOException{
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/LogIn.fxml"));
+        Parent root = loader.load();
+
+        // Otvaranje nove scene s drugim prozorom
+        Scene scene = new Scene(root);
+        Stage stage = new Stage();
+        stage.setScene(scene);
+        stage.show();
+    }
 
     public void PayInvoiceButton(ActionEvent actionEvent) throws IOException {
         // Code to handle save menu item
