7bccffa3198c67e730ec1bc10767fee0b83de320
nmahic1 <nmahic1@etf.unsa.ba>
2023-05-22 19:29:20 +0200

    user defined exception in login controller

--- src/main/java/controller/LogInController.java
+++ src/main/java/controller/LogInController.java
@@ -56,7 +56,7 @@ public class LogInController {
     }
 }
 */
-
+/*
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -127,6 +127,94 @@ public class LogInController {
         for (LoginObserver observer : observers) {
             observer.onLoginSuccess();
         }
+    }
+}
+
+interface LoginObserver {
+    void onLoginSuccess();
+}
+*/
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Label;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class LogInController {
+    private List<LoginObserver> observers = new ArrayList<>();
+
+    @FXML
+    private TextField usernameField;
+
+    @FXML
+    private PasswordField passwordField;
+
+    @FXML
+    private Label errorLabel;
+
+    @FXML
+    private Button loginButton;
+
+    @FXML
+    void login(ActionEvent event) {
+        String username = usernameField.getText();
+        String password = passwordField.getText();
+
+        try {
+            // Provjera korisničkog imena i lozinke
+            if (isValidCredentials(username, password)) {
+                notifyObservers();
+
+                // Učitavanje FXML datoteke drugog prozora
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("/BrandAndProduct.fxml"));
+                Parent root = loader.load();
+
+                // Otvaranje nove scene s drugim prozorom
+                Scene scene = new Scene(root);
+                Stage stage = new Stage();
+                stage.setScene(scene);
+                stage.show();
+            } else {
+                throw new InvalidCredentialsException("Invalid username or password");
+            }
+        } catch (InvalidCredentialsException e) {
+            errorLabel.setText(e.getMessage());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private boolean isValidCredentials(String username, String password) {
+        return username.equals("nmahic1") && password.equals("12345");
+    }
+
+    public void addObserver(LoginObserver observer) {
+        observers.add(observer);
+    }
+
+    public void removeObserver(LoginObserver observer) {
+        observers.remove(observer);
+    }
+
+    private void notifyObservers() {
+        for (LoginObserver observer : observers) {
+            observer.onLoginSuccess();
+        }
+    }
+}
+
+class InvalidCredentialsException extends Exception {
+    public InvalidCredentialsException(String message) {
+        super(message);
     }
 }
 
