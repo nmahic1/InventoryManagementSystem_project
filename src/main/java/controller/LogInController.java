381770398908a8ba26718600a8bead0e5c23bb08
nmahic1 <nmahic1@etf.unsa.ba>
2023-05-22 18:45:27 +0200

    adding observable pattern in login controller

--- src/main/java/controller/LogInController.java
+++ src/main/java/controller/LogInController.java
@@ -1,4 +1,5 @@
 package controller;
+/*
 //package org.example;
 
 import javafx.fxml.FXML;
@@ -53,5 +54,83 @@ public class LogInController {
             errorLabel.setText("Invalid username or password");
         }
     }
+}
+*/
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Label;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class LogInController {
+    private List<LoginObserver> observers = new ArrayList<>();
+
+    @FXML
+    private TextField usernameField;
+
+    @FXML
+    private PasswordField passwordField;
+
+    @FXML
+    private Label errorLabel;
+
+    @FXML
+    private Button loginButton;
+
+    @FXML
+    void login(ActionEvent event) {
+        String username = usernameField.getText();
+        String password = passwordField.getText();
+
+        // Provjera korisničkog imena i lozinke
+        if (username.equals("nmahic1") && password.equals("12345")) {
+            notifyObservers();
+
+            try {
+                // Učitavanje FXML datoteke drugog prozora
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("/BrandAndProduct.fxml"));
+                Parent root = loader.load();
+
+                // Otvaranje nove scene s drugim prozorom
+                Scene scene = new Scene(root);
+                Stage stage = new Stage();
+                stage.setScene(scene);
+                stage.show();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+            // Prikazivanje upozorenja ako korisničko ime ili lozinka nisu ispravni
+            errorLabel.setText("Invalid username or password");
+        }
+    }
+
+    public void addObserver(LoginObserver observer) {
+        observers.add(observer);
+    }
+
+    public void removeObserver(LoginObserver observer) {
+        observers.remove(observer);
+    }
+
+    private void notifyObservers() {
+        for (LoginObserver observer : observers) {
+            observer.onLoginSuccess();
+        }
+    }
+}
+
+interface LoginObserver {
+    void onLoginSuccess();
 }
 
