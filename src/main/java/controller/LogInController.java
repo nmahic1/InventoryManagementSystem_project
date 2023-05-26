a8380247ce73a86dbd8646e64d1ffd75483b17b6
nmahic1 <nmahic1@etf.unsa.ba>
2023-05-26 12:16:33 +0200

    making DB for login

--- src/main/java/controller/LogInController.java
+++ src/main/java/controller/LogInController.java
@@ -1,139 +1,5 @@
 package controller;
-/*
-//package org.example;
 
-import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
-import javafx.scene.control.Label;
-import javafx.event.ActionEvent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-
-import java.io.IOException;
-
-public class LogInController {
-
-    @FXML
-    private TextField usernameField;
-
-    @FXML
-    private PasswordField passwordField;
-
-    @FXML
-    private Label errorLabel;
-
-    @FXML
-    private Button loginButton;
-
-    @FXML
-    void login(ActionEvent event) {
-        String username = usernameField.getText();
-        String password = passwordField.getText();
-
-        // Provjera korisničkog imena i lozinke
-        if (username.equals("nmahic1") && password.equals("12345")) {
-            try {
-                // Učitavanje FXML datoteke drugog prozora
-                FXMLLoader loader = new FXMLLoader(getClass().getResource("/BrandAndProduct.fxml"));
-                Parent root = loader.load();
-
-                // Otvaranje nove scene s drugim prozorom
-                Scene scene = new Scene(root);
-                Stage stage = new Stage();
-                stage.setScene(scene);
-                stage.show();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        } else {
-            // Prikazivanje upozorenja ako korisničko ime ili lozinka nisu ispravni
-            errorLabel.setText("Invalid username or password");
-        }
-    }
-}
-*/
-/*
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
-import javafx.scene.control.Label;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-
-public class LogInController {
-    private List<LoginObserver> observers = new ArrayList<>();
-
-    @FXML
-    private TextField usernameField;
-
-    @FXML
-    private PasswordField passwordField;
-
-    @FXML
-    private Label errorLabel;
-
-    @FXML
-    private Button loginButton;
-
-    @FXML
-    void login(ActionEvent event) {
-        String username = usernameField.getText();
-        String password = passwordField.getText();
-
-        // Provjera korisničkog imena i lozinke
-        if (username.equals("nmahic1") && password.equals("12345")) {
-            notifyObservers();
-
-            try {
-                // Učitavanje FXML datoteke drugog prozora
-                FXMLLoader loader = new FXMLLoader(getClass().getResource("/BrandAndProduct.fxml"));
-                Parent root = loader.load();
-
-                // Otvaranje nove scene s drugim prozorom
-                Scene scene = new Scene(root);
-                Stage stage = new Stage();
-                stage.setScene(scene);
-                stage.show();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        } else {
-            // Prikazivanje upozorenja ako korisničko ime ili lozinka nisu ispravni
-            errorLabel.setText("Invalid username or password");
-        }
-    }
-
-    public void addObserver(LoginObserver observer) {
-        observers.add(observer);
-    }
-
-    public void removeObserver(LoginObserver observer) {
-        observers.remove(observer);
-    }
-
-    private void notifyObservers() {
-        for (LoginObserver observer : observers) {
-            observer.onLoginSuccess();
-        }
-    }
-}
-
-interface LoginObserver {
-    void onLoginSuccess();
-}
-*/
 //new code
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -154,10 +20,10 @@ import java.util.List;
 
 import java.sql.Connection;
 import repository.LoginDBConnectionRepository;
-import java.sql.Statement;
+
 
 public class LogInController {
-    private List<LoginObserver> observers = new ArrayList<>();
+    //private List<LoginObserver> observers = new ArrayList<>();
 
     @FXML
     private TextField usernameField;
@@ -171,7 +37,7 @@ public class LogInController {
     @FXML
     private Button loginButton;
 
-    /*
+
     public void login(ActionEvent e) throws IOException {
         if(usernameField.getText().isBlank()==false && passwordField.getText().isBlank()==false){
           //  errorLabel.setText("try again");
@@ -222,9 +88,10 @@ public class LogInController {
 
     }
 
-//pattern
-     */
-    @FXML
+     }
+
+    //pattern
+    /*@FXML
     void login(ActionEvent event) {
         String username = usernameField.getText();
         String password = passwordField.getText();
@@ -279,12 +146,9 @@ class InvalidCredentialsException extends Exception {
         super(message);
     }
 
-
-
 }
-
 
 interface LoginObserver {
     void onLoginSuccess();
 }
-
+*/
