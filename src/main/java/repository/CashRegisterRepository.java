8f89d1a803fbc4f8f4a6c2058b3090bb1b3a36d0
nmahic1 <nmahic1@etf.unsa.ba>
2023-05-28 18:52:19 +0200

    DB fixing

--- src/main/java/repository/CashRegisterRepository.java
+++ src/main/java/repository/CashRegisterRepository.java
@@ -56,13 +56,11 @@ public class CashRegisterRepository {
 
 
 import controller.CashRegisterController;
+import controller.ProductController;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class CashRegisterRepository {
     private ObservableList<CashRegisterController.Data> dataListCashRegister;
@@ -84,27 +82,30 @@ public class CashRegisterRepository {
         return dataListCashRegister;
     }
 
-    private void insertDataIntoDatabase(CashRegisterController.Data data) {
-        try {
-            // Establish a database connection
-            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/ims?useSSL=false", "root", "12345");
+    private static Connection getConnection() throws SQLException {
+        String url = "jdbc:mysql://localhost:3306/ims?useSSL=false";
+        String username = "root";
+        String password = "12345";
+        return DriverManager.getConnection(url, username, password);
+    }
 
-            // Prepare the SQL statement
-            String query = "INSERT INTO cashregister (barcode, name, quantity, price) VALUES (?, ?, ?, ?)";
+    private void insertDataIntoDatabase(CashRegisterController.Data data) {
+        try (Connection connection = getConnection()) {
+            String query = "INSERT INTO cashregister (quantity, barcode, name, price)" +" VALUES (?, ?, ?, ?)";
             PreparedStatement statement = connection.prepareStatement(query);
-            statement.setInt(1, data.getBarcode());
-            statement.setString(2, data.getBrand());
-            statement.setInt(3, data.getQuantity());
+            statement.setInt(1, data.getQuantity());
+            statement.setInt(2, data.getBarcode());
+            statement.setString(3, data.getBrand());
             statement.setInt(4, data.getRetailPrice());
 
-            // Execute the statement
             statement.executeUpdate();
-
-            // Close the resources
             statement.close();
-            connection.close();
         } catch (SQLException e) {
             e.printStackTrace();
         }
     }
+
+
 }
+
+
--- src/main/java/service/CashRegisterService.java
+++ src/main/java/service/CashRegisterService.java
@@ -21,6 +21,7 @@ public class CashRegisterService {
         cashRegisterRepository.removeData(data);
     }
 
+
 }
 
 
