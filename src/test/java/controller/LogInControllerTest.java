c75403b72bec23beaf530b5c1c7192a3da05de40
nmahic1 <nmahic1@etf.unsa.ba>
2023-06-02 18:59:37 +0200

    making first junit test

---
+++ src/test/java/controller/LogInControllerTest.java
@@ -0,0 +1,14 @@
+package controller;
+
+import static org.junit.Assert.*;
+
+public class LogInControllerTest {
+
+    @org.junit.Test
+    public void login() {
+    }
+
+    @org.junit.Test
+    public void validateLogin() {
+    }
+}
