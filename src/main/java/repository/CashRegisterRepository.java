daebf3b740ab956081ea831eb565258fa15f2895
nmahic1 <nmahic1@etf.unsa.ba>
2023-06-03 15:17:23 +0200

    writing .properties file

--- src/main/java/repository/CashRegisterRepository.java
+++ src/main/java/repository/CashRegisterRepository.java
@@ -2,6 +2,9 @@ package repository;
 import controller.CashRegisterController;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import java.util.Properties;
+import java.io.IOException;
+import java.io.InputStream;
 
 
 import java.sql.*;
@@ -26,12 +29,31 @@ public class CashRegisterRepository {
         return dataListCashRegister;
     }
 
-    private static Connection getConnection() throws SQLException {
+   /* private static Connection getConnection() throws SQLException {
         String url = "jdbc:mysql://localhost:3306/ims?useSSL=false";
         String username = "root";
         String password = "12345";
         return DriverManager.getConnection(url, username, password);
     }
+
+    */
+
+    private static Connection getConnection() throws SQLException {
+        Properties properties = new Properties();
+
+        try (InputStream inputStream = CashRegisterRepository.class.getClassLoader().getResourceAsStream("config.properties")) {
+            properties.load(inputStream);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        String url = properties.getProperty("db.url");
+        String username = properties.getProperty("db.username");
+        String password = properties.getProperty("db.password");
+
+        return DriverManager.getConnection(url, username, password);
+    }
+
 
     private void insertDataIntoDatabase(CashRegisterController.Data data) {
         try (Connection connection = getConnection()) {
