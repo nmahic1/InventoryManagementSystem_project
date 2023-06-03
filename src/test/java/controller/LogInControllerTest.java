f13269e46b37b90842f992accce041d61b2421a4
nmahic1 <nmahic1@etf.unsa.ba>
2023-06-03 14:48:39 +0200

    Writing tests

--- src/test/java/controller/LogInControllerTest.java
+++ src/test/java/controller/LogInControllerTest.java
@@ -1,11 +1,21 @@
 package controller;
 
+import org.junit.Before;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import repository.LoginDBConnectionRepository;
+import service.LoginService;
+
+
+import java.util.Arrays;
+import java.util.Collection;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class LogInControllerTest {
 
-    @Test
+ /*   @Test
     public void login() {
 
     }
@@ -13,4 +23,16 @@ public class LogInControllerTest {
     @Test
     public void validateLogin() {
     }
+}
+
+  */
+
+    @Test
+    public void testValidCredentials() {
+        LoginService loginService = new LoginService();
+        boolean result = loginService.validateLogin("nmahic1", "12345");
+        assertTrue(result);
+
+    }
+
 }
