9ce2bdaff2bb7c1f18d02dcfa66f3aab9ed380cd
nmahic1 <nmahic1@etf.unsa.ba>
2023-06-03 17:16:21 +0200

    making DB cashregister work

--- src/main/java/repository/CashRegisterRepository.java
+++ src/main/java/repository/CashRegisterRepository.java
@@ -1,17 +1,14 @@
 package repository;
 import controller.CashRegisterController;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 
 import java.io.FileInputStream;
 import java.util.Properties;
-import java.io.IOException;
-import java.io.InputStream;
 
 
 import java.sql.*;
 
 public class CashRegisterRepository {
+    /*
     private ObservableList<CashRegisterController.Data> dataListCashRegister;
 
     public CashRegisterRepository() {
@@ -20,7 +17,7 @@ public class CashRegisterRepository {
 
     public void addData(CashRegisterController.Data data) {
         dataListCashRegister.add(data);
-        insertDataIntoDatabase(data);
+       // insertDataIntoDatabase(data);
     }
 
     public void removeData(CashRegisterController.Data data) {
@@ -30,7 +27,9 @@ public class CashRegisterRepository {
     public ObservableList<CashRegisterController.Data> getDataList() {
         return dataListCashRegister;
     }
+*/
 
+    //do ovog dijela
    /* private static Connection getConnection() throws SQLException {
         String url = "jdbc:mysql://localhost:3306/ims?useSSL=false";
         String username = "root";
@@ -89,6 +88,42 @@ public class CashRegisterRepository {
         return databaseLink;
     }
 
+
+    public void addProduct(CashRegisterController.Data cashregister) {
+        try (Connection connection = getConnection()) {
+           // String query = "INSERT INTO cashregister (barcode, quantity, name, price) " +"VALUES (?, ?, ?, ?)";
+            String query = "INSERT INTO cashregister (barcode, quantity, name, price) " +
+                    "SELECT ?, ?, brand, ? FROM product WHERE barcode = ?";
+
+            PreparedStatement statement = connection.prepareStatement(query);
+            statement.setInt(1, cashregister.getBarcode());
+            statement.setInt(2, cashregister.getQuantity());
+            statement.setString(3, cashregister.getBrand());
+            statement.setInt(4, cashregister.getRetailPrice());
+
+
+            statement.executeUpdate();
+            statement.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void deleteProduct(CashRegisterController.Data cashregister) {
+        try (Connection connection = getConnection()) {
+            String query = "DELETE FROM cashregister WHERE id=?";
+
+            PreparedStatement statement = connection.prepareStatement(query);
+            statement.setInt(1, cashregister.getId());
+
+            statement.executeUpdate();
+            statement.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /*
     private void insertDataIntoDatabase(CashRegisterController.Data data) {
         try (Connection connection = getConnection()) {
             String query = "INSERT INTO cashregister (barcode, quantity, name, price) VALUES (?, ?, ?, ?)";
@@ -114,7 +149,7 @@ public class CashRegisterRepository {
     }
 
 
-
+*/
 }
 
 
