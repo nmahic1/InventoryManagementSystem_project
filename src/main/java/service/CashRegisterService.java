5de5f6a878215e8fb4bb46947d8a75c97010613b
nmahic1 <nmahic1@etf.unsa.ba>
2023-06-03 17:15:52 +0200

    making DB cashregister work

--- src/main/java/service/CashRegisterService.java
+++ src/main/java/service/CashRegisterService.java
@@ -3,14 +3,15 @@ import controller.CashRegisterController;
 import repository.CashRegisterRepository;
 
 
+
 public class CashRegisterService {
-    private CashRegisterRepository cashRegisterRepository;
+   /* private CashRegisterRepository cashRegisterRepository;
 
     public CashRegisterService() {
         cashRegisterRepository = new CashRegisterRepository();
     }
 
-    public void addData(int quantity, String brand, int barcode, int retailprice) {
+    public void addData(int quantity, String brand, int barcode, int price) {
         CashRegisterController.Data data = new CashRegisterController.Data(barcode,  quantity);
         cashRegisterRepository.addData(data);
     }
@@ -18,11 +19,21 @@ public class CashRegisterService {
     public void removeData(CashRegisterController.Data data) {
         cashRegisterRepository.removeData(data);
     }
+*/
 
+    private CashRegisterRepository cashregisterRepository;
 
+    public CashRegisterService() {
+        this.cashregisterRepository = new CashRegisterRepository();
+    }
 
+    public void addProduct(CashRegisterController.Data cashregister) {
+        cashregisterRepository.addProduct(cashregister);
+    }
 
-
+    public void deleteProduct(CashRegisterController.Data cashregister) {
+        cashregisterRepository.deleteProduct(cashregister);
+    }
 
 
 }
