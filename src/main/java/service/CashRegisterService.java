781e6d7e0e5ffaa9b2038c6044698c8d786cc591
nmahic1 <nmahic1@etf.unsa.ba>
2023-06-04 13:30:59 +0200

    deleting commented code

--- src/main/java/service/CashRegisterService.java
+++ src/main/java/service/CashRegisterService.java
@@ -2,24 +2,7 @@ package service;
 import controller.CashRegisterController;
 import repository.CashRegisterRepository;
 
-
-
 public class CashRegisterService {
-   /* private CashRegisterRepository cashRegisterRepository;
-
-    public CashRegisterService() {
-        cashRegisterRepository = new CashRegisterRepository();
-    }
-
-    public void addData(int quantity, String brand, int barcode, int price) {
-        CashRegisterController.Data data = new CashRegisterController.Data(barcode,  quantity);
-        cashRegisterRepository.addData(data);
-    }
-
-    public void removeData(CashRegisterController.Data data) {
-        cashRegisterRepository.removeData(data);
-    }
-*/
 
     private CashRegisterRepository cashregisterRepository;
 
