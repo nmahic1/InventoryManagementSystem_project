d13ad88ac494ade5b4689f7bf71af6e7c80b6dcb
nmahic1 <nmahic1@etf.unsa.ba>
2023-06-05 17:47:43 +0200

    Junit tests for cashregister

---
+++ src/test/java/service/CashRegisterServiceTest.java
@@ -0,0 +1,13 @@
+package service;
+
+import controller.CashRegisterController;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import repository.CashRegisterRepository;
+import java.util.ArrayList;
+import java.util.List;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CashRegisterServiceTest {
+
+}
