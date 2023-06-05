34c2c2da0e07de3b1eb603a612d1d0675e96953b
nmahic1 <nmahic1@etf.unsa.ba>
2023-06-05 16:37:41 +0200

    Junit tests for login

--- src/main/java/service/LoginService.java
+++ src/main/java/service/LoginService.java
@@ -30,6 +30,10 @@ public class LoginService {
         this.loginRepo = new LoginDBConnectionRepository();
     }
 
+    public LoginService(LoginDBConnectionRepository loginRepo) {
+        this.loginRepo=loginRepo;
+    }
+
     public boolean validateLogin(String username, String password) {
         // Ovdje možete dodati dodatnu poslovnu logiku, provjeru i validaciju
         // npr. provjeru duljine korisničkog imena/lozinke, enkripciju itd.
