298dac3769c7470aaf9df4e41fe311eecd47fffd
nmahic1 <nmahic1@etf.unsa.ba>
2023-06-06 14:32:10 +0200

    Junit tests for cashregister

--- src/main/java/service/CashRegisterService.java
+++ src/main/java/service/CashRegisterService.java
@@ -4,12 +4,18 @@ import repository.CashRegisterRepository;
 
 public class CashRegisterService {
 
+
     private CashRegisterRepository cashregisterRepository;
+   // private CashRegisterRepository cashRegisterRepository;
 
     public CashRegisterService() {
         this.cashregisterRepository = new CashRegisterRepository();
     }
 
+    //dodano za testove
+    public CashRegisterRepository getCashRegisterRepository() {
+        return cashregisterRepository;
+    }
 
     public void addProduct(CashRegisterController.Data cashregister) {
         cashregisterRepository.addProduct(cashregister);
