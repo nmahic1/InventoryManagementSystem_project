75fa183c3308c649311d292621a8d54f20afdfa2
nmahic1 <nmahic1@etf.unsa.ba>
2023-06-08 19:14:24 +0200

    Writing test using mockito

--- src/test/java/service/LoginServiceTest.java
+++ src/test/java/service/LoginServiceTest.java
@@ -73,6 +73,9 @@ public class LoginServiceTest {
     public void validateLogin() {
     }
 }*/
+
+//MOJ KOD
+/*
 import org.junit.Before;
 import org.junit.Test;
 import repository.LoginDBConnectionRepository;
@@ -134,4 +137,150 @@ public class LoginServiceTest {
 
 
 
+}
+
+ */
+
+/*
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import repository.LoginDBConnectionRepository;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.when;
+
+public class LoginServiceTest {
+    @Mock
+    private LoginDBConnectionRepository loginRepo;
+    private LoginService loginService;
+
+    @Before
+    public void setup() {
+        MockitoAnnotations.initMocks(this);
+        loginService = new LoginService(loginRepo);
+    }
+
+    @Test
+    public void testValidateLogin_ValidCredentials_ReturnsTrue() {
+        // Arrange
+        String username = "nmahic1";
+        String password = "12345";
+
+        // Mock behavior
+        when(loginRepo.validateLogin(username, password)).thenReturn(true);
+
+        // Act
+        boolean isValid = loginService.validateLogin(username, password);
+
+        // Assert
+        assertTrue(isValid);
+    }
+
+    @Test
+    public void testValidateLogin_InvalidCredentials_ReturnsFalse() {
+        // Arrange
+        String username = "nmahic";
+        String password = "123456";
+
+        // Mock behavior
+        when(loginRepo.validateLogin(username, password)).thenReturn(false);
+
+        // Act
+        boolean isValid = loginService.validateLogin(username, password);
+
+        // Assert
+        assertFalse(isValid);
+    }
+
+    @Test
+    public void testValidateLogin_BlankCredentials_ReturnsFalse() {
+        // Arrange
+        String username = "";
+        String password = "";
+
+        // Mock behavior
+        when(loginRepo.validateLogin(username, password)).thenReturn(false);
+
+        // Act
+        boolean isValid = loginService.validateLogin(username, password);
+
+        // Assert
+        assertFalse(isValid);
+    }
+}
+
+
+ */
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import repository.LoginDBConnectionRepository;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.when;
+
+public class LoginServiceTest {
+    @Mock
+    private LoginDBConnectionRepository loginRepo;
+    private LoginService loginService;
+
+    @BeforeEach
+    public void setup() {
+        MockitoAnnotations.openMocks(this);
+        loginService = new LoginService(loginRepo);
+    }
+
+    @Test
+    public void testValidateLogin_ValidCredentials_ReturnsTrue() {
+        // Arrange
+        String username = "nmahic1";
+        String password = "12345";
+
+        // Mock behavior
+        when(loginRepo.validateLogin(username, password)).thenReturn(true);
+
+        // Act
+        boolean isValid = loginService.validateLogin(username, password);
+
+        // Assert
+        assertTrue(isValid);
+    }
+
+    @Test
+    public void testValidateLogin_InvalidCredentials_ReturnsFalse() {
+        // Arrange
+        String username = "nmahic";
+        String password = "123456";
+
+        // Mock behavior
+        when(loginRepo.validateLogin(username, password)).thenReturn(false);
+
+        // Act
+        boolean isValid = loginService.validateLogin(username, password);
+
+        // Assert
+        assertFalse(isValid);
+    }
+
+    @Test
+    public void testValidateLogin_BlankCredentials_ReturnsFalse() {
+        // Arrange
+        String username = "";
+        String password = "";
+
+        // Mock behavior
+        when(loginRepo.validateLogin(username, password)).thenReturn(false);
+
+        // Act
+        boolean isValid = loginService.validateLogin(username, password);
+
+        // Assert
+        assertFalse(isValid);
+    }
 }
