3daab3a166a073afe6424eac4d128bb5d287e3b8
nmahic1 <nmahic1@etf.unsa.ba>
2023-06-08 19:16:03 +0200

    Writing test using mockito

--- src/test/java/service/LoginServiceTest.java
+++ src/test/java/service/LoginServiceTest.java
@@ -214,7 +214,7 @@ public class LoginServiceTest {
 
 
  */
-
+/*
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
@@ -283,4 +283,46 @@ public class LoginServiceTest {
         // Assert
         assertFalse(isValid);
     }
+}
+
+ */
+
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import repository.LoginDBConnectionRepository;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.when;
+
+public class LoginServiceTest {
+    @Mock
+    private LoginDBConnectionRepository loginRepo;
+    private LoginService loginService;
+
+    @BeforeEach
+    public void setup() {
+        MockitoAnnotations.openMocks(this);
+        loginService = new LoginService(loginRepo);
+    }
+
+    @Test
+    public void testValidateLogin_ValidCredentials_ReturnsTrue() {
+        // Arrange
+        String username = "nmahic1";
+        String password = "12345";
+
+        // Mock behavior
+        when(loginRepo.validateLogin(username, password)).thenReturn(true);
+
+        // Act
+        boolean isValid = loginService.validateLogin(username, password);
+
+        // Assert
+        assertTrue(isValid);
+    }
+
 }
