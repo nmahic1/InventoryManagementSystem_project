5f66e07c0a3085881ef5d756a0dd64170cc4463e
nmahic1 <nmahic1@etf.unsa.ba>
2023-06-13 16:08:58 +0200

    code reformation

--- src/main/java/controller/MenuBarController.java
+++ src/main/java/controller/MenuBarController.java
@@ -23,44 +23,45 @@ public class MenuBarController {
 
     @FXML
     private Menu logOut;
-        @FXML
-        public static void handleProduct(ActionEvent event) throws IOException {
-            // Code to handle save menu item
-            FXMLLoader loader = new FXMLLoader(MenuBarController.class.getResource("/Product.fxml"));
-            Parent root = loader.load();
 
-            // Opening a new scene with another window
-            Scene scene = new Scene(root);
-            Stage stage = new Stage();
-            stage.setScene(scene);
-            stage.show();
-        }
+    @FXML
+    public static void handleProduct(ActionEvent event) throws IOException {
+        // Code to handle save menu item
+        FXMLLoader loader = new FXMLLoader(MenuBarController.class.getResource("/Product.fxml"));
+        Parent root = loader.load();
 
-        @FXML
-        public static void handleCashRegister(ActionEvent event) throws IOException {
-            // Code to handle exit menu item
-            FXMLLoader loader = new FXMLLoader(MenuBarController.class.getResource("/CashRegister.fxml"));
-            Parent root = loader.load();
+        // Opening a new scene with another window
+        Scene scene = new Scene(root);
+        Stage stage = new Stage();
+        stage.setScene(scene);
+        stage.show();
+    }
 
-            // Opening a new scene with another window
-            Scene scene = new Scene(root);
-            Stage stage = new Stage();
-            stage.setScene(scene);
-            stage.show();
-        }
+    @FXML
+    public static void handleCashRegister(ActionEvent event) throws IOException {
+        // Code to handle exit menu item
+        FXMLLoader loader = new FXMLLoader(MenuBarController.class.getResource("/CashRegister.fxml"));
+        Parent root = loader.load();
 
-        @FXML
-        public static void logout(ActionEvent actionEvent) throws IOException {
+        // Opening a new scene with another window
+        Scene scene = new Scene(root);
+        Stage stage = new Stage();
+        stage.setScene(scene);
+        stage.show();
+    }
 
-            FXMLLoader loader = new FXMLLoader(MenuBarController.class.getResource("/LogIn.fxml"));
-            Parent root = loader.load();
+    @FXML
+    public static void logout(ActionEvent actionEvent) throws IOException {
 
-            // Opening a new scene with another window
-            Scene scene = new Scene(root);
-            Stage stage = new Stage();
-            stage.setScene(scene);
-            stage.show();
-        }
+        FXMLLoader loader = new FXMLLoader(MenuBarController.class.getResource("/LogIn.fxml"));
+        Parent root = loader.load();
+
+        // Opening a new scene with another window
+        Scene scene = new Scene(root);
+        Stage stage = new Stage();
+        stage.setScene(scene);
+        stage.show();
+    }
 
 
 }
