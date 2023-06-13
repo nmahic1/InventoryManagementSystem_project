3189c988b287247e7cd8d9d59ed2d9353b4cf3f2
nmahic1 <nmahic1@etf.unsa.ba>
2023-06-13 19:01:47 +0200

    adding patterns

---
+++ src/main/java/InventoryMain.java
@@ -0,0 +1,7 @@
+public class InventoryMain {
+    public static void main(String[] args) {
+        IMSApp.observers.add(new InventoryPrinter());
+        IMSApp.productInputStrategy = new ConsoleProductInputStrategy();
+        IMSApp.main(args);
+    }
+}
