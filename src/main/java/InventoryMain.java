7540399838a3da5b132b84ae6c9c897a052e7fe3
nmahic1 <nmahic1@etf.unsa.ba>
2023-06-13 19:43:27 +0200

    changes

--- src/main/java/InventoryMain.java
+++ src/main/java/InventoryMain.java
@@ -1,5 +1,6 @@
 public class InventoryMain {
     public static void main(String[] args) {
+
         IMSApp.observers.add(new InventoryPrinter());
         IMSApp.productInputStrategy = new ConsoleProductInputStrategy();
         IMSApp.main(args);
