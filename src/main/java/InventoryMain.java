d859c53286ba302b933eca7a937ac552a46c9c40
nmahic1 <nmahic1@etf.unsa.ba>
2023-06-23 15:06:47 +0200

    Adding comments for javaDoc

--- src/main/java/InventoryMain.java
+++ src/main/java/InventoryMain.java
@@ -1,4 +1,13 @@
+/**
+ * Glavna klasa za pokretanje Inventory aplikacije.
+ */
 public class InventoryMain {
+    /**
+     * Metoda koja se poziva pri pokretanju aplikacije.
+     * Inicijalizira promatrače, strategiju unosa proizvoda i pokreće Inventory aplikaciju.
+     *
+     * @param args Argumenti komandne linije
+     */
     public static void main(String[] args) {
 
         IMSApp.observers.add(new InventoryPrinter());
