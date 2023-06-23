33e43deee0cc4febb558fc8e45cc247f298b86b4
nmahic1 <nmahic1@etf.unsa.ba>
2023-06-23 15:05:59 +0200

    Adding comments for javaDoc

--- src/main/java/Javafx.java
+++ src/main/java/Javafx.java
@@ -5,8 +5,19 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
+
+/**
+ * Glavna klasa za pokretanje JavaFX aplikacije.
+ */
 public class Javafx extends Application {
 
+    /**
+     * Metoda koja se poziva pri pokretanju aplikacije.
+     * Postavlja korijenski element i prikazuje glavni prozor.
+     *
+     * @param stage Stage objekt za prikaz prozora
+     * @throws Exception Izuzetak koji se može javiti pri učitavanju korijenskog elementa iz FXML datoteke
+     */
     @Override
     public void start(Stage stage) throws Exception {
 
@@ -16,6 +27,11 @@ public class Javafx extends Application {
         stage.show();
     }
 
+    /**
+     * Glavna metoda za pokretanje JavaFX aplikacije.
+     *
+     * @param args Argumenti komandne linije
+     */
     public static void main(String[] args) {
         launch(args);
     }
